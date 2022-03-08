6a55d01a630d392acdc76a095d5aefe87efa7b76
=Oguzhan <=oguzhantemizer1@gmail.com>
2022-03-08 19:17:35 +0300

    tests files

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -26,6 +27,20 @@ var theTest = []struct {
 	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
 	{"sa", "/make-reservation", "GET", []postData{}, http.StatusOK},
 	{"ms", "/major-suite", "GET", []postData{}, http.StatusOK},
+	{"post-search-avail", "/search-availability", "POST", []postData{
+		{key: "start", value: "2020-01-01"},
+		{key: "end", value: "2020-01-02"},
+	}, http.StatusOK},
+	{"post-search-avail-json", "/search-availability-json", "POST", []postData{
+		{key: "start", value: "2020-01-01"},
+		{key: "end", value: "2020-01-02"},
+	}, http.StatusOK},
+	{"make-reservation post", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "John"},
+		{key: "last_name", value: "Smith"},
+		{key: "email", value: "me@here.com"},
+		{key: "phone", value: "555-555-5555"},
+	}, http.StatusOK},
 }
 
 func TestHandlers(t *testing.T) {
@@ -36,6 +51,20 @@ func TestHandlers(t *testing.T) {
 	for _, e := range theTest {
 		if e.method == "GET" {
 			resp, err := ts.Client().Get(ts.URL + e.url)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Error(fmt.Sprintf("for %s, expected %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode))
+			}
+		} else {
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
 			if err != nil {
 				t.Log(err)
 				t.Fatal(err)
