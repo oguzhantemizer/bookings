a817dc37ff45c663418e4435b8f69ef9b33fe018
=Oguzhan <oguzhntemizer@gmail.com>
2022-03-10 13:00:47 +0300

    forms.go test file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/oguzhantemizer/bookings/internal/config"
 	"github.com/oguzhantemizer/bookings/internal/handlers"
+	"github.com/oguzhantemizer/bookings/internal/helpers"
 	"github.com/oguzhantemizer/bookings/internal/models"
 	"github.com/oguzhantemizer/bookings/internal/render"
 
@@ -19,6 +21,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main is the main function
 func main() {
@@ -49,6 +53,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	// set up the session
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
@@ -71,6 +81,8 @@ func run() error {
 	handlers.NewHandlers(repo)
 
 	render.NewTemplates(&app)
+
+	helpers.NewHelpers(&app)
 
 	return nil
 }
