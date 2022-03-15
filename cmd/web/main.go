6c548ff620b9d181241c53c63eb455e1e0915acc
=Oguzhan <oguzhntemizer@gmail.com>
2022-03-15 23:12:16 +0300

    some codes cleaned

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -51,6 +51,9 @@ func run() (*driver.DB, error) {
 
 	//what am i going to put in the session
 	gob.Register(models.Reservation{})
+	gob.Register(models.User{})
+	gob.Register(models.Room{})
+	gob.Register(models.Restriction{})
 
 	// change this to true when in production
 	app.InProduction = false
@@ -90,7 +93,7 @@ func run() (*driver.DB, error) {
 	repo := handlers.NewRepo(&app, db)
 	handlers.NewHandlers(repo)
 
-	render.NewTemplates(&app)
+	render.NewRenderer(&app)
 
 	helpers.NewHelpers(&app)
 
