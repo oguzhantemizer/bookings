f30e0d207188d9cee5f5c9d4ae1ad654cb71e4b9
=Oguzhan <oguzhntemizer@gmail.com>
2022-03-15 22:57:45 +0300

    models package updated

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,69 @@
 package models
 
+import "time"
+
 //Reservation holds reservatioın data
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+//Users is the user model
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+//Rooms is the room model
+type Rooms struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+//Restrictions is the restriction model
+type Restrictions struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+//Reservations is the reservation model
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomID    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Rooms
+}
+
+//RoomRestriction is the restriction model
+type RoomRestrictions struct {
+	ID            int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomID        int
+	Room          Rooms
+	ReservationID int
+	Reservation   Reservations
+	RestrictionID int
+	Restriction   Restrictions
 }
