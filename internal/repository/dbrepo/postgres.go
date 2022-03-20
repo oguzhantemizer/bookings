72572e460e66da0247253d534a467421f01e57a2
=Oguzhan <oguzhntemizer@gmail.com>
2022-03-20 22:29:24 +0300

    room availability database query

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -62,3 +62,32 @@ func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
 	}
 	return nil
 }
+
+//SearchAvailabilityByDates returns true if availability exist, and false if no availability exist
+func (m *postgresDBRepo) SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var numRows int
+
+	query := `
+	select
+		count(id)
+	from 
+		room_restrictions
+	where
+		room_id = $1
+		$2<end_date and $3> start_date;`
+
+	row := m.DB.QueryRowContext(ctx, query, roomID, start, end)
+	err := row.Scan(&numRows)
+	if err != nil {
+		return false, err
+	}
+
+	if numRows == 0 {
+		return true, nil
+	}
+	return false, nil
+
+}
